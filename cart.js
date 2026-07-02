9b0759c880dd7adbd78ec1f19ff3f45153aecd58
Roits <mohitsinghth9@gmail.com>
2026-07-02 11:02:53 +0530

    Improve cart UI and mobile responsiveness

--- cart.js
+++ cart.js
@@ -1,32 +1,45 @@
 const cartItems = document.querySelector("#cart-items");
-const cartTotal = document.querySelector("#cart-total");
-
 function renderCart() {
+
     if(cart.length === 0) {
-        cartItems.innerHTML = "<p>Your cart is empty.</p>";
-        cartTotal.innerHTML = "";
+        cartItems.innerHTML = "<h2>Your cart is empty.</h2>";
+
+        document.getElementById("subtotal").innerText = "₹0";
+        document.getElementById("summary-total").innerText = "₹0";
         return;
     }
 
     let html = "";
     let total = 0;
 
-    for(let i = 0; i < cart.length; i++) {
-        total += cart[i].price * cart[i].quantity;
-        html += `
-        <div class="cart-item">
-            <img src="${cart[i].image}" width="100px">
-            <div>
-                <h4>${cart[i].name}</h4>
-                <p>${cart[i].brand}</p>
-                <p>₹${cart[i].price} x ${cart[i].quantity}</p>
-                <button onclick="removeFromCart(${cart[i].id})">Remove</button>
-            </div>
-        </div>`;
-    }
+   for(let i = 0; i < cart.length; i++) {
+
+    total += cart[i].price * cart[i].quantity;
+
+    html += `
+    <div class="cart-item">
+
+        <img src="${cart[i].image}" alt="${cart[i].name}">
+
+        <div class="cart-info">
+            <h4>${cart[i].name}</h4>
+            <p><strong>${cart[i].brand}</strong></p>
+            <p>Price: ₹${cart[i].price}</p>
+            <p>Quantity: ${cart[i].quantity}</p>
+            <p><strong>Subtotal: ₹${cart[i].price * cart[i].quantity}</strong></p>
+
+            <button onclick="removeFromCart(${cart[i].id})">
+                Remove
+            </button>
+        </div>
+
+    </div>`;
+}
 
     cartItems.innerHTML = html;
-    cartTotal.innerHTML = `<h3>Total: ₹${total}</h3>`;
+
+    document.getElementById("subtotal").innerText = `₹${total}`;
+    document.getElementById("summary-total").innerText = `₹${total}`;
 }
 
 function removeFromCart(id) {
